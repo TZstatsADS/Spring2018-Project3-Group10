c3aea341044d0afd3da2587ed9c3b1acec253b8e
wenyuangu <35541652+wenyuangu@users.noreply.github.com>
2018-03-18 14:10:05 -0400

    v2

---
+++ doc/Sift+RF.R
@@ -0,0 +1,39 @@
+library(randomForest, lib.loc = "C:\\Users\\Administrator\\AppData\\Local\\Temp\\RtmpgN0kEC\\downloaded_packages" )
+library(caret, lib.loc = "C:\\Users\\Administrator\\AppData\\Local\\Temp\\RtmpgN0kEC\\downloaded_packages")
+library(e1071, lib.loc = "C:\\Users\\Administrator\\AppData\\Local\\Temp\\RtmpgN0kEC\\downloaded_packages")
+
+##
+setwd("~/GitHub/Spring2018-Project3-spring2018-project3-group10/doc")
+sift <- read.csv('../data/SIFT_train.csv',header = F)
+sift <- sift[,-1]
+dim(sift)
+
+features <- sift
+labels_set <- read.csv('../data/label_train.csv')
+labels <- labels_set[,3]
+dataset <- cbind(labels,features)
+dim(dataset)
+colnames(dataset)
+
+#--------------Caret Package for Tuning Parameters-------------
+#----------------Two Parameters: mtry & ntree------------------
+
+customRF <- list(type = "Classification", library = "randomForest", loop = NULL)
+customRF$parameters <- data.frame(parameter = c("mtry", "ntree"), class = rep("numeric", 2), label = c("mtry", "ntree"))
+customRF$grid <- function(x, y, len = NULL, search = "grid") {}
+customRF$fit <- function(x, y, wts, param, lev, last, weights, classProbs, ...) {
+  randomForest(x, y, mtry = param$mtry, ntree=param$ntree, ...)
+}
+customRF$predict <- function(modelFit, newdata, preProc = NULL, submodels = NULL)
+  predict(modelFit, newdata)
+customRF$prob <- function(modelFit, newdata, preProc = NULL, submodels = NULL)
+  predict(modelFit, newdata, type = "prob")
+customRF$sort <- function(x) x[order(x[,1]),]
+customRF$levels <- function(x) x$classes
+
+control <- trainControl(method="repeatedcv", number=5, repeats=2) # 5-fold cross validation and 2 repeats
+tunegrid <- expand.grid(.mtry=c(5:15), .ntree=c(1000, 1500, 2000, 2500))
+set.seed(123)
+custom <- train(as.factor(labels)~., data=dataset, method=customRF, tuneGrid=tunegrid, trControl=control)
+summary(custom)
+plot(custom)
