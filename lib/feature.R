f0b878de170a560d781143f071f9672484b4e41c
Yiyi Zhang <35751914+yiyi-zhang-cu@users.noreply.github.com>
2018-03-20 11:14:52 -0400

    comment out resize image

--- lib/feature.R
+++ lib/feature.R
@@ -17,8 +17,7 @@ library("OpenImageR")
 ################################################################################
 ### BEGIN SIFT ###
 
-feature_SIFT <- function(img_dir, 
-                         width = 20, height = 20){
+feature_SIFT <- function(img_dir){
   ### Construct process features for training/testing images
   ### Sample simple feature: Extract row average raw pixel values as features
   
@@ -27,13 +26,13 @@ feature_SIFT <- function(img_dir,
   
   n_files <- length(list.files(img_dir))
   img0 <-  readImage(paste0(img_dir, list.files(img_dir)[1], sep = ""))
-  img0 <- EBImage::resize(img0, width, height)
+#  img0 <- EBImage::resize(img0, width, height)
   
   ### store vectorized pixel values of images
   sift <- matrix(NA, n_files, nrow(as.matrix(img0))) 
   for(i in 1:n_files){
     img <- readImage(paste0(img_dir, list.files(img_dir)[i], sep = ""))
-    img <- EBImage::resize(img, width, height)
+#    img <- EBImage::resize(img, width, height)
     sift[i,] <- rowMeans(img)
   }
   return(sift)
@@ -45,8 +44,9 @@ feature_SIFT <- function(img_dir,
 ################################################################################
 ### BEGIN HOG ###
 
-feature_HOG <- function(img_dir, n_hogs = 54, 
-                        width = 20, height = 20){
+feature_HOG <- function(img_dir, n_hogs = 54
+#                        ,width = 20, height = 20
+                        ){
   ### Construct process features for training/testing images
   
   ### Input: a directory that contains images ready for processing
@@ -56,7 +56,7 @@ feature_HOG <- function(img_dir, n_hogs = 54,
   hog <- matrix(NA, n_files, n_hogs)
   for (i in 1:n_files){
     img <- readImage(paste0(img_dir, list.files(img_dir)[i], sep = ""))
-    img <- EBImage::resize(img, width, height)
+#    img <- EBImage::resize(img, width, height)
     hog[i,] <- HOG(img)
   }
   return(hog)
